6ffde408e315d157252cb84e54d0c7836669d9ea
ceveloper7 <cevabarcvilla@gmail.com>
2026-02-06 16:55:06 -0300

    Ruta .NET - Curso Basico C# - class & objects - static keyword

---
+++ CursoCSharp/Static/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Static {
+    class People {
+        // propiedad de clase
+        private static int count = 0;
+        // propiedades de objetos
+        private string name;
+        private int age;
+
+        public People() {
+            count++;
+        }
+
+        public string Name
+        {
+            get { return name; } 
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public static string GetCount()
+        {
+            return $"Esta clase se ha utilizado {count} veces";
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            People people = new People();
+            people.Name = "Mario";
+            people.Age = 33;
+
+            People antonio = new People() {
+                Name="Antonio",
+                Age=40
+            };
+
+            Console.WriteLine(People.GetCount());
+        }
+    }
+}
