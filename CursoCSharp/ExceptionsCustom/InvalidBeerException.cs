0547512f6fdb60371f53a0497086f0a26922b962
ceveloper7 <cevabarcvilla@gmail.com>
2026-02-07 12:04:04 -0300

    Ruta C# .NET - Curso C# - Manejo de Exceptions - Custom Exceptions

---
+++ CursoCSharp/ExceptionsCustom/InvalidBeerException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsCustom
+{
+    public class InvalidBeerException : Exception
+    {
+        public InvalidBeerException() : base("Invalid Beer creation: Beer doesnt has name or brand"){ }
+    }
+}
---
+++ CursoCSharp/ExceptionsCustom/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExceptionsCustom
+{
+    class Program
+    {
+        public class Beer {
+            private string name;
+            private string brand;
+
+            public string Name
+            {
+                get { return name; } 
+                set { name = value; }
+            }
+
+            public string Brand
+            {
+                get { return brand; }
+                set { brand = value; }
+            }
+
+            public override string ToString()
+            {
+                if (name == null || brand == null) {
+                    throw new InvalidBeerException();
+                }
+
+                return $"Beer: {Name}, Brand: {Brand}";
+            }
+        }
+
+        static void Main(string[] args) {
+            try
+            {
+                Beer beer = new Beer();
+                beer.Name = "London Porter";
+
+                // provocamos una exception
+                //beer.Brand = "Fuller's";
+
+                Console.WriteLine(beer);
+            }
+            catch (InvalidBeerException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
