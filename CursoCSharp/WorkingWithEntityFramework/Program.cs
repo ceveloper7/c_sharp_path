a5227e3a2fa2906d4bb15eeb5753d6ce70af5d27
ceveloper7 <cevabarcvilla@gmail.com>
2026-02-08 17:08:59 -0300

    Ruta C# .NET - Curso Fundamentos C# - Entity Framework - Working with Entity framework

---
+++ CursoCSharp/WorkingWithEntityFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿using DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace WorkingWithEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            DbContextOptionsBuilder<CsharpdbContext> optionsBuilder = new DbContextOptionsBuilder<CsharpdbContext>();
+            optionsBuilder.UseSqlServer("Server=HOME\\SQLEXPRESS; Database=csharpdb; User=sa; Password=b4000$.; Trusted_Connection=True; TrustServerCertificate=True");
+
+            // Crear la conexion a la BD
+            using (CsharpdbContext ctx = new CsharpdbContext(optionsBuilder.Options)) 
+            {
+                // get all beers
+                var beers = ctx.Beers.ToList();
+                foreach (var beer in beers)
+                    Console.WriteLine($"Beer name: {beer.Name}, Brand id: {beer.BrandId}");
+            }
+           
+        }
+    }
+}
