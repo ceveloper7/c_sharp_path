d98b32c25a4cf9bc3fd56f2ea946a9f961a233cc
ceveloper7 <cevabarcvilla@gmail.com>
2026-02-08 09:41:16 -0300

    Ruta C# .NET - Curso Fundamentos C# - Working with Database - CRUD Sample System

--- CursoCSharp/DatabaseConnection/Program.cs
+++ CursoCSharp/DatabaseConnection/Program.cs
@@ -4,18 +4,54 @@ namespace DatabaseConnection
 {
     class Program
     {
+        public static void showMenu() {
+            Console.WriteLine("\nSelecciona una opcion");
+            Console.WriteLine("=====================");
+            Console.WriteLine("1. Get all beers");
+            Console.WriteLine("2. Add a new Beer");
+            Console.WriteLine("3. Edit beer information");
+            Console.WriteLine("4. Delete a beer");
+            Console.WriteLine("5. Exit from System");
+            Console.WriteLine();
+        }
+
+        public static void getAllBeerData(BeerDB beerDb) {
+            Console.Clear();
+            Console.WriteLine("Getting all Beers from Database...");
+            List<Beer> beers = beerDb.GetAll();
+            foreach (Beer beer in beers)
+                Console.WriteLine($"Beer: {beer.Id}, {beer.Name}");
+
+            Console.WriteLine();
+        }
+
         static void Main(string[] args) {
             try
             {
                 // el @ permite que \ sea tomado como parte de la cadena
                 BeerDB beerDb = new BeerDB(@"HOME\SQLEXPRESS", "csharpdb", "sa", "b4000$.");
-                beerDb.Connect();
 
-                List<Beer> beers = beerDb.GetAll();
-                foreach (Beer beer in beers)
-                    Console.WriteLine($"Beer: {beer.Id}, {beer.Name}");
+                bool again = true;
+                int op = 0;
 
-                beerDb.Close();
+                do
+                {
+                    showMenu();
+                    Console.WriteLine("Choose an option...");
+                    op = int.Parse(Console.ReadLine());
+
+                    switch (op) {
+                        case 1:
+                            getAllBeerData(beerDb);
+                            break;
+                        case 5:
+                            again = false;
+                            break;
+                    }
+
+                }while (again);
+
+              
             }
             catch (Exception ex) {
                 Console.WriteLine(ex);
