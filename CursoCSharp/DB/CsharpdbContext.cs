33e2f8f4070d671347d1d9cef76dcf80020a037e
ceveloper7 <cevabarcvilla@gmail.com>
2026-02-08 13:00:39 -0300

    Ruta C# .NET - Curso Fundamentos C# - Entity Framework - Installing and Mapping Database Tables to Project DB

---
+++ CursoCSharp/DB/CsharpdbContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DB;
+
+public partial class CsharpdbContext : DbContext
+{
+    public CsharpdbContext()
+    {
+    }
+
+    public CsharpdbContext(DbContextOptions<CsharpdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beer> Beers { get; set; }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=HOME\\SQLEXPRESS; Database=csharpdb; User=sa; Password=b4000$.; Trusted_Connection=True; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Beer>(entity =>
+        {
+            entity.ToTable("beer");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.BrandId).HasColumnName("brandId");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Beers)
+                .HasForeignKey(d => d.BrandId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_beer_brand");
+        });
+
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.ToTable("brand");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
