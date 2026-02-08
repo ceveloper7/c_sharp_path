210394d9e231553c10bd1eb5167a4ec085c9ac07
ceveloper7 <cevabarcvilla@gmail.com>
2026-02-08 00:36:24 -0300

    Ruta C# .NET - Curso C# - Database Connection

---
+++ CursoCSharp/DatabaseConnection/DB.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseConnection
+{
+    public class DB
+    {
+        private string _connectionString;
+        protected SqlConnection _connection;
+
+        // parametros requeridos para construir una conexion basica a un base de datos
+        public DB(string server, string dbName, string user, string password) {
+            _connectionString = $"Data Source={server}; Initial Catalog={dbName}; User={user}; Password={password}; TrustServerCertificate=True";
+        }
+
+        public void Connect() {
+            _connection = new SqlConnection(_connectionString); 
+            _connection.Open();
+        }
+
+        public void Close() {
+            if(_connection != null && _connection.State == System.Data.ConnectionState.Open) 
+                _connection.Close();
+        }
+    }
+}
---
+++ CursoCSharp/DatabaseConnection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DatabaseConnection
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            // el @ permite que \ sea tomado como parte de la cadena
+            DB db = new DB(@"HOME\SQLEXPRESS", "csharpdb", "sa", "b4000$.");
+            db.Connect();
+
+            db.Close();
+        }
+    }
+}
